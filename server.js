8823f5fc047b8b139b5fb0f059477d68200e96ac
Alexgioff <gioffre.alex@outlook.it>
2019-03-07 03:01:09 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/data', data);
 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+
+    app.use(express.static(path.join(__dirname, 'client/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + '/client/build/index.html'))
     });
-}
+
 
 const PORT = process.env.PORT || 5000;
 
